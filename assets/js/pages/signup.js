ce74acab8fc7e43099678ba1a7ab38f0a865debc
hubintheroot <hubintheroot@gmail.com>
2025-07-24 16:47:54 +0900

    feat: signup 버튼 클릭 상호작용 기능 구현

---
+++ assets/js/pages/signup.js
@@ -0,0 +1,21 @@
+(() => {
+  const tabs = document.querySelector('.signup__tabs');
+  const btns = tabs.querySelectorAll('button');
+  const sellerField = document.querySelector('.signup__group--seller');
+  tabs.addEventListener('click', (e) => {
+    if (e.target.tagName !== 'BUTTON') return;
+    btns.forEach((btn, idx) => {
+      btn.classList.remove('left', 'right', 'signup__tabs-button--active');
+    });
+
+    if (e.target === btns[0]) {
+      btns[0].classList.add('left', 'signup__tabs-button--active');
+      btns[1].classList.add('right');
+    } else {
+      btns[1].classList.add('right', 'signup__tabs-button--active');
+      btns[0].classList.add('left');
+    }
+
+    sellerField.classList.toggle('hide', e.target.id !== 'sellerTab');
+  });
+})();
