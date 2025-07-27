d1d5b130e7e011751b4156f1792e96e5e5e4e82e
Liebe_Chaeso <d131215@naver.com>
2025-07-27 12:49:48 +0900

    feature/api-fetch 전 커밋

--- assets/js/pages/login.js
+++ assets/js/pages/login.js
@@ -1,19 +1,25 @@
 // 변수 선언
 const btn = document.querySelector('.login-box__submit');
 
-btn.addEventListener('click', (e) => {
-    e.preventDefault();
-
-    const loginId = document.getElementById('login-id').value;
+function validatation() {
+    const loginId = document.getElementById('loginId').value;
     const loginPw = document.getElementById('login-password').value;
 
     if (loginId.trim() === '') {
         console.error('아이디가 입력되지 않았습니다.');
-        document.getElementById('login-id').focus();
+        document.getElementById('loginId').focus();
+        return;
     }
 
     if (loginPw.trim() === '') {
         console.error('비밀번호가 입력되지 않았습니다.');
         document.getElementById('login-password').focus();
+        return;
     }
+}
+
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+
+    validatation();
 });
