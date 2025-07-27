564ad6333903953a14c5387ede44c2b857e9de81
woori ko <mynameiswooriko@gmail.com>
2025-07-27 23:35:09 +0900

    feat: 유효성 검사 함수 추가: ID, 비밀번호, 핸드폰 번호 검증 로직 구현

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -1,1 +1,35 @@
 // Form validation scripts
+
+/**
+ * @param {string} id - 20자 이내의 영문 대소문자 및 숫자
+ * @returns {boolean}
+ */
+
+export function validateId(id) {
+    const idRegex = /^[a-zA-Z0-9]{1,20}$/;
+    let isOk = idRegex.test(id);
+    return isOk;
+}
+
+/**
+ * @param {string} password - 8자 이상이어야 하며, 영문 대소문자, 숫자, 특수문자를 모두 포함
+ * @returns {boolean}
+ */
+
+export function validatePassword(password) {
+    const passwordRegex =
+        /^(?=.*[a-zA-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/;
+    let isOk = passwordRegex.test(password);
+    return isOk;
+}
+
+/**
+ * @param {string} phone - 일반 휴대폰 형식
+ * @returns {boolean}
+ */
+
+export function validatePhoneNumber(phone) {
+    const phoneRegex = /^(010|011|016|017|018|019)\d{7,8}$/;
+    let isOk = phoneRegex.test(phone);
+    return isOk;
+}
