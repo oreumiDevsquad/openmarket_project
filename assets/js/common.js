4c64402b81b9b0ae1fca490c9fabcf0df8ed540a
hubintheroot <hubintheroot@gmail.com>
2025-07-28 11:50:14 +0900

    feat: 모달 컴포넌트 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,4 +1,3 @@
-(async () => {})();
 document.addEventListener('DOMContentLoaded', async () => {
     // 1. data-component 속성을 가진 모든 요소는 컴포넌트로 간주하고 모으기
     const components = document.querySelectorAll('[data-component]');
@@ -31,3 +30,71 @@ document.addEventListener('DOMContentLoaded', async () => {
         }
     }
 });
+
+const modal = {
+    open: openModal,
+};
+
+function openModal(
+    message,
+    agreeMsg,
+    disagreeMsg,
+    agreeAction = () => alert('수락버튼 눌림'),
+    disagreeAction = () => alert('거절버튼 눌림')
+) {
+    // 이미 모달이 열려있다면 작동 안함
+    const existingModal = document.getElementById('commonModal');
+    if (existingModal) return;
+    // 모달 요소 생성
+    const modal = document.createElement('div');
+    modal.setAttribute('id', 'commonModal');
+    modal.classList.add('modal');
+    // 모달 배경 누르면 삭제
+    modal.addEventListener('click', (e) => {
+        if (e.target === e.currentTarget) modal.remove();
+    });
+
+    const container = document.createElement('div');
+    container.classList.add('modal__content');
+
+    // 닫기 버튼 생성
+    const closeBtn = document.createElement('button');
+    closeBtn.setAttribute('type', 'button');
+    closeBtn.classList.add('modal__close-button');
+    closeBtn.innerHTML = `<img src="../assets/icons/icon-delete.svg" alt="닫기" />`;
+
+    // 닫기 버튼 클릭 시 모달 제거
+    closeBtn.addEventListener('click', () => {
+        modal.remove();
+    });
+    const messageElement = document.createElement('p');
+    messageElement.classList.add('modal__message');
+    messageElement.textContent = message || '문구는 변경됩니다.';
+
+    const buttonGroup = document.createElement('div');
+    buttonGroup.classList.add('modal__button-group');
+
+    // 확인 및 취소 버튼 생성
+    const cancelButton = document.createElement('button');
+    cancelButton.classList.add('modal__button', 'modal__button--cancel');
+    cancelButton.setAttribute('type', 'button');
+    cancelButton.textContent = disagreeMsg || '아니오';
+    cancelButton.addEventListener('click', disagreeAction);
+
+    const confirmButton = document.createElement('button');
+    confirmButton.classList.add('modal__button', 'modal__button--confirm');
+    confirmButton.setAttribute('type', 'button');
+    confirmButton.textContent = agreeMsg || '예';
+    confirmButton.addEventListener('click', agreeAction);
+
+    // 추가
+    buttonGroup.appendChild(cancelButton);
+    buttonGroup.appendChild(confirmButton);
+
+    container.appendChild(closeBtn);
+    container.appendChild(messageElement);
+    container.appendChild(buttonGroup);
+
+    modal.appendChild(container);
+    document.body.appendChild(modal);
+}
