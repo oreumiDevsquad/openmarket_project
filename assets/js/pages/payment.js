2b600c159c9a60fb6d7f100ae2960510c2e00e58
hubintheroot <hubintheroot@gmail.com>
2025-07-29 00:11:06 +0900

    feat: 장바구니 정보 바탕 주문 정보 렌더링

--- assets/js/pages/payment.js
+++ assets/js/pages/payment.js
@@ -1,14 +1,67 @@
-// 임시 로그인 상태 유지하기
+import { AuthAPI } from '../auth.js';
+import { formatPrice } from '../utils.js';
 (async () => {
-    const { API } = await import('../api.js');
-    const loginData = await API.login('t1', 'xldnjsWkd1!');
-    const accessToken = loginData.access;
-    const refreshToken = loginData.refresh;
-    const data = loginData.user;
+    // 페이지 접속 시 한 번만 실행하는 장바구니 목록 불러오기라서 즉시 실행함수 사용
+    const $orderListTbody = document.querySelector('.order-list__table tbody');
+    const $totalPrice = document.querySelector('.order-list__total-price span');
 
-    // 장바구니 확인
-    const myCartData = await API.getCartList(accessToken);
+    const myCartData = await AuthAPI.getCartList();
     const myCartList = myCartData.results;
 
-    console.log(myCartList);
+    let totalPrice = 0;
+
+    const frag = document.createDocumentFragment();
+
+    myCartList.forEach(async (item) => {
+        const tr = document.createElement('tr');
+        const amount = item.product.price * item.quantity;
+        const shipping_fee = item.product.shipping_fee;
+        totalPrice += shipping_fee + amount;
+        tr.innerHTML = `<td class="order-list__product-info">
+                                <div class="order-list__item">
+                                    <img
+                                        src="${item.product.image}"
+                                        alt="${item.product.info}"
+                                        class="order-list__image"
+                                    />
+                                    <div class="order-list__details">
+                                        <p class="order-list__brand">
+                                            ${item.product.seller.store_name}
+                                        </p>
+                                        <h4 class="order-list__name">
+                                            ${item.product.info}
+                                        </h4>
+                                        <p class="order-list__quantity">
+                                            수량: ${item.quantity}개
+                                        </p>
+                                    </div>
+                                </div>
+                            </td>
+                            <td class="order-list__discount">
+                                <div class="order-list__wrapper">
+                                    <span class="order-list__discount-value"
+                                        >-</span
+                                    >
+                                </div>
+                            </td>
+                            <td class="order-list__shipping">
+                                <div class="order-list__wrapper">
+                                    <span class="order-list__shipping-value"
+                                        >${shipping_fee > 0 ? shipping_fee + '원' : '무료배송'}</span
+                                    >
+                                </div>
+                            </td>
+                            <td class="order-list__amount">
+                                <div class="order-list__wrapper">
+                                    <strong class="order-list__price"
+                                        >${amount + shipping_fee}원</strong
+                                    >
+                                </div>
+                            </td>`;
+
+        frag.appendChild(tr);
+    });
+
+    $orderListTbody.appendChild(frag);
+    $totalPrice.textContent = formatPrice(totalPrice) + '원';
 })();
