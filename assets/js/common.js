b2e710cc2fb6515e4d92de1d68e8c7c8ed54bee4
hubintheroot <hubintheroot@gmail.com>
2025-07-28 16:05:24 +0900

    feat: 모달 기능 추가

--- assets/js/common.js
+++ assets/js/common.js
@@ -31,70 +31,140 @@ document.addEventListener('DOMContentLoaded', async () => {
     }
 });
 
-const modal = {
-    open: openModal,
-};
-
-function openModal(
-    message,
-    agreeMsg,
-    disagreeMsg,
-    agreeAction = () => alert('수락버튼 눌림'),
-    disagreeAction = () => alert('거절버튼 눌림')
-) {
+/**
+ * @param {number} count - 현재 장바구니 수량(기본 값: 1)
+ * @param {string} type - 모달 타입, login, delete, quantity 중 하나
+ * @param {function} confirmAction - confirm 버튼에 바인딩 할 함수
+ *
+ * @returns
+ */
+export function openModal({
+    count = 1,
+    type = 'message',
+    confirmAction = () => alert('confirm'),
+}) {
+    console.log(type);
     // 이미 모달이 열려있다면 작동 안함
     const existingModal = document.getElementById('commonModal');
     if (existingModal) return;
     // 모달 요소 생성
     const modal = document.createElement('div');
-    modal.setAttribute('id', 'commonModal');
+    modal.setAttribute('role', 'dialog');
+    modal.setAttribute('aria-modal', 'true');
+    modal.setAttribute('aria-labelledby', 'modal-title');
     modal.classList.add('modal');
+
     // 모달 배경 누르면 삭제
     modal.addEventListener('click', (e) => {
         if (e.target === e.currentTarget) modal.remove();
     });
 
+    // 모달 내용 컨테이너 생성
     const container = document.createElement('div');
-    container.classList.add('modal__content');
+    container.classList.add('modal-content');
 
     // 닫기 버튼 생성
     const closeBtn = document.createElement('button');
     closeBtn.setAttribute('type', 'button');
-    closeBtn.classList.add('modal__close-button');
-    closeBtn.innerHTML = `<img src="../assets/icons/icon-delete.svg" alt="닫기" />`;
+    closeBtn.setAttribute('aria-label', '닫기');
+    closeBtn.classList.add('modal__close-btn');
+    closeBtn.innerHTML = `<img src="../assets/icons/icon-delete.svg" alt="" />`;
 
     // 닫기 버튼 클릭 시 모달 제거
     closeBtn.addEventListener('click', () => {
         modal.remove();
     });
-    const messageElement = document.createElement('p');
-    messageElement.classList.add('modal__message');
-    messageElement.textContent = message || '문구는 변경됩니다.';
 
-    const buttonGroup = document.createElement('div');
-    buttonGroup.classList.add('modal__button-group');
+    // TODO:
+    // 모달 내용에 따라 다른 구성 요소 생성
+    const contentBox = document.createElement('div');
 
-    // 확인 및 취소 버튼 생성
-    const cancelButton = document.createElement('button');
-    cancelButton.classList.add('modal__button', 'modal__button--cancel');
-    cancelButton.setAttribute('type', 'button');
-    cancelButton.textContent = disagreeMsg || '아니오';
-    cancelButton.addEventListener('click', disagreeAction);
+    const actionBox = document.createElement('div');
+    actionBox.classList.add('modal__actions');
+    const confirmBtn = document.createElement('button');
+    const cancelBtn = document.createElement('button');
 
-    const confirmButton = document.createElement('button');
-    confirmButton.classList.add('modal__button', 'modal__button--confirm');
-    confirmButton.setAttribute('type', 'button');
-    confirmButton.textContent = agreeMsg || '예';
-    confirmButton.addEventListener('click', agreeAction);
+    confirmBtn.classList.add('btn', 'btn--confirm');
+    cancelBtn.classList.add('btn', 'btn--cancel');
 
-    // 추가
-    buttonGroup.appendChild(cancelButton);
-    buttonGroup.appendChild(confirmButton);
+    confirmBtn.addEventListener('click', confirmAction);
+    cancelBtn.addEventListener('click', (e) => {
+        modal.remove();
+    });
 
+    switch (type) {
+        case 'quantity':
+            container.classList.add('modal__quantity');
+            contentBox.classList.add('modal__quantity-con');
+            const increaseBtn = document.createElement('button');
+            const input = document.createElement('input');
+            const decreaseBtn = document.createElement('button');
+
+            decreaseBtn.classList.add('quantity-btn');
+            increaseBtn.setAttribute('type', 'button');
+            increaseBtn.setAttribute('aria-label', '수량 감소');
+
+            input.setAttribute('type', 'number');
+            input.setAttribute('value', '1');
+            input.setAttribute('min', '1');
+            input.setAttribute('max', '99');
+            input.readOnly = true;
+            input.setAttribute('aria-label', '수량');
+            input.value = count;
+
+            increaseBtn.classList.add('quantity-btn');
+            increaseBtn.setAttribute('type', 'button');
+            increaseBtn.setAttribute('aria-label', '수량증가');
+
+            increaseBtn.innerHTML = `<img
+                                src="./../assets/icons/icon-plus-line.svg"
+                                alt=""
+                            />`;
+            decreaseBtn.innerHTML = `<img
+                                src="./../assets/icons/icon-minus-line.svg"
+                                alt=""
+                            />`;
+            decreaseBtn.addEventListener('click', () => {
+                if (input.value > 1) input.value--;
+            });
+            increaseBtn.addEventListener('click', () => {
+                if (input.value < 99) input.value++;
+            });
+            confirmBtn.textContent = '수정';
+            cancelBtn.textContent = '취소';
+
+            contentBox.appendChild(decreaseBtn);
+            contentBox.appendChild(input);
+            contentBox.appendChild(increaseBtn);
+            break;
+
+        case 'delete':
+            container.classList.add('modal__delete');
+            contentBox.classList.add('modal__delete-con');
+            contentBox.innerHTML = `<p>상품을 삭제하시겠습니까?</p>`;
+            confirmBtn.textContent = '확인';
+            cancelBtn.textContent = '취소';
+            break;
+        case 'login':
+            container.classList.add('modal__login-msg');
+            contentBox.classList.add('modal__login-msg-con');
+            contentBox.innerHTML = `<p>로그인이 필요한 서비스입니다.<br />
+                            로그인 하시겠습니까?</p>`;
+            confirmBtn.textContent = '예';
+            cancelBtn.textContent = '아니오';
+            break;
+
+        default:
+            console.error(
+                '모달의 타입을 확인하세요, login, delete, quantity 중 하나여야 합니다.'
+            );
+            break;
+    }
     container.appendChild(closeBtn);
-    container.appendChild(messageElement);
-    container.appendChild(buttonGroup);
-
+    container.appendChild(contentBox);
+    actionBox.appendChild(cancelBtn);
+    actionBox.appendChild(confirmBtn);
+    container.appendChild(actionBox);
     modal.appendChild(container);
     document.body.appendChild(modal);
 }
