3bef2c06363ffb18b39c72785ccb37e033d7dae8
Liebe_Chaeso <d131215@naver.com>
2025-07-29 14:23:22 +0900

    feat:products 요소에 달러 처리

--- assets/js/pages/products.js
+++ assets/js/pages/products.js
@@ -13,15 +13,15 @@ import { formatPrice, calculatePrice } from '../utils.js';
     const targetProduct = products.results.find((product) => product.id === id);
 
     // 요소 변수 선언
-    const image = document.querySelector('.product-detail__image');
-    const sellerStore = document.querySelector('.product-detail__seller-name');
-    const productName = document.querySelector('.product-detail__name');
-    const price = document.querySelector('.product-detail__price');
+    const $image = document.querySelector('.product-detail__image');
+    const $sellerStore = document.querySelector('.product-detail__seller-name');
+    const $productName = document.querySelector('.product-detail__name');
+    const $price = document.querySelector('.product-detail__price');
 
     // 총 상품 금액 및 총 수량 변수
     const $totalQuantity = document.querySelector('.quantity__input');
     // 수량 -> 어떻게 불러오지?
-    const totalPriceSection = document.querySelector('.total-section__price');
+    const $totalPriceSection = document.querySelector('.total-section__price');
 
     // 가격 및 포맷 적용
     const format = formatPrice(targetProduct.price);
@@ -32,9 +32,9 @@ import { formatPrice, calculatePrice } from '../utils.js';
     const totalFormat = formatPrice(totalPrice);
 
     // id와 일치상품 렌더링
-    image.setAttribute('src', `${targetProduct.image}`);
-    sellerStore.textContent = targetProduct.seller.store_name;
-    productName.textContent = targetProduct.name;
-    price.textContent = format;
-    totalPriceSection.textContent = totalFormat;
+    $image.setAttribute('src', `${targetProduct.image}`);
+    $sellerStore.textContent = targetProduct.seller.store_name;
+    $productName.textContent = targetProduct.name;
+    $price.textContent = format;
+    $totalPriceSection.textContent = totalFormat;
 })();
